ac0f755f63cdd09c8d6c776794cab48b1f003b2b
rodrigomsrocha <rodrigomtc82@gmail.com>
2021-09-30 18:55:56 -0300

    avoiding duplicated user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,14 +12,19 @@ export default NextAuth({
       scope: "read:user",
     }),
   ],
-  jwt: {
-    signingKey: process.env.SIGNIN_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user;
       try {
-        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
         return true;
       } catch {
         return false;
